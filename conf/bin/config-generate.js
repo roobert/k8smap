4934c29e062d473cb6397642fd5596e77b4f2361
Rob Wilson <roobert@gmail.com>
2018-09-06 13:04:59 +0100

    base64 decode secret

--- conf/bin/config-generate.js
+++ conf/bin/config-generate.js
@@ -45,7 +45,7 @@ try {
     secrets = JSON.parse(secretsResult)
 
     secret = secrets.items.find(function (obj) { return /^default.*/.test(obj.metadata.name) });
-    apiToken = secret.data.token
+    apiToken = new Buffer(secret.data.token, 'base64').toString()
 
     clusterData = kubeConfig.clusters.find(function (obj) { return obj.name === clusterKey });
     apiAddress = clusterData['cluster']['server']
