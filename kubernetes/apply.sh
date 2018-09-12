94fc3502786083cf81ddc085901e2d37ff874c4c
Rob Wilson <roobert@gmail.com>
2018-09-12 16:27:37 +0100

    update kubernetes deployment

--- kubernetes/apply.sh
+++ kubernetes/apply.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 
 kubectl apply -f secret.yaml
 kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f ingress.yaml
--- kubernetes/secret.sh
+++ kubernetes/secret.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 VUE_CONFIG_FILE="${1:-../conf/config.vue.json}"
-NGINX_CONFIG_FILE="${1:../conf/nginx.conf.docker}"
+NGINX_CONFIG_FILE="${1:-../conf/nginx.conf.docker}"
 
 set -euo pipefail
 
-sed "s/VUE_CONFIG/`cat ${VUE_CONFIG_FILE}|base64 -w0`/g" secret.yaml.tmpl > secret.yaml
-sed "s/NGINX_CONFIG/`cat ${NGINX_CONFIG_FILE}|base64 -w0`/g" secret.yaml.tmpl > secret.yaml
+NGINX_CONFIG=$(cat ${NGINX_CONFIG_FILE} | base64 -w0)
+VUE_CONFIG=$(cat ${VUE_CONFIG_FILE} | base64 -w0)
+
+cp -v secret.yaml.tmpl secret.yaml
+
+echo "  nginx.conf: $NGINX_CONFIG" >> secret.yaml
+echo "  config.vue.json: $VUE_CONFIG" >> secret.yaml
+
+#sed "s/VUE_CONFIG/`cat ${VUE_CONFIG_FILE}|base64 -w0`/g" secret.yaml.tmpl > secret.yaml
+#sed "s/NGINX_CONFIG/$(cat ${NGINX_CONFIG_FILE}|base64 -w0)/g" secret.yaml.tmpl > secret.yaml
