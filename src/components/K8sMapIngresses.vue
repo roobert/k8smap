325297c669f40bf2018947b512900a7f649a6653
Rob Wilson <roobert@gmail.com>
2018-09-24 09:18:13 +0100

    formatting

--- src/components/K8sMapIngresses.vue
+++ src/components/K8sMapIngresses.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
     <div v-show="displayIngress(ingress)"
-      class="ingress col-sm border"
+      class="ingress row border"
 
       v-for="ingress in ingresses.data.items"
       v-bind:key="ingress.metadata.uid"
       v-bind:ingress="ingress"
       @click.capture="$store.commit('panelText', ingress)"
     >
-      <div class="title">{{ ingress.metadata.name }}</div>
-      <div
-        v-for="address in ingress.status.loadBalancer.ingress"
-        v-bind:key="address.ip"
-        v-bind:address="address"
-      >
-        {{ address }}
-      <div class="metadata"><pre>{{ ingress }}</pre></div>
+      <div class="col">
+        <div class="title">{{ ingress.metadata.name }}</div>
+        <div class="metadata"><pre>{{ ingress }}</pre></div>
+      </div>
+
+      <div class="col-2 addresses">
+        <div
+          v-for="address in ingress.status.loadBalancer.ingress"
+          v-bind:key="address.ip"
+          v-bind:address="address"
+        >
+          <code>{{ address.ip }}</code>
+        </div>
       </div>
     </div>
   </div>
@@ -62,9 +67,15 @@ export default {
 
 <style>
 .ingress {
-  padding: 10px;
+  padding: 10px 0px;
   margin-top: 10px;
+  margin-left: 0px;
+  margin-right: 0px;
   background: #e6f2ff;
+}
+
+.addresses {
+  text-align: right ! important;
 }
 
 .image {
@@ -114,4 +125,5 @@ export default {
 .image {
   border: 1px solid lightgrey ! important;
 }
+
 </style>
